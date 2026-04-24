2b90a2de025cd98819e2f0edfcbd0bb823fbf2ea
ryou6 <r.koga3426@gmail.com>
2026-04-24 19:52:34 +0900

    Add files via upload

---
+++ kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,21 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	
+	Dictionary_Chapter21 jisho = new Dictionary_Chapter21 ();{
+		
+	}
+	String [] words = {"apple","banana","grape","orange"};
+	{
+		
+	
+for (int i = 0; i < words.length; i++) {
+    jisho.sesrch(words[i]);
+}
+}
+}
+}
